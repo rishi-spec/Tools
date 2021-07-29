58c99e18da92fb862d703e2b9187895fe18dc310
unknown <rushikeshwankhade17@gmail.com>
2021-07-29 10:24:15 +0530

    second commit

--- src/main/java/com/shree/ToolsApplication.java
+++ src/main/java/com/shree/ToolsApplication.java
@@ -2,12 +2,13 @@ package com.shree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ToolsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToolsApplication.class, args);
+		ConfigurableApplicationContext ctxt = SpringApplication.run(ToolsApplication.class, args);
 	}
 
 }
